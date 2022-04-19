ce5d62b88df6e864d64b08bf12df10addbf97a03
unconsciousness13 <pakounicredit@gmail.com>
2022-04-19 13:18:50 +0200

    third upload

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,45 @@
+from statistics import mode
+from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset() .filter(status='published')
+
+    OPTIONS = (
+        ('draft', 'Draft'),
+        ('published', 'Punlished')
+    )
+
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, default=1
+    )
+    title = models.CharField(max_length=250)
+    excerpt = models.CharField(max_length=250, null=True)
+    content = models.SlugField(max_length=250, unique_for_date='published')
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts'
+    )
+    status = models.CharField(
+        max_length=10, choices=OPTIONS, default='punlished')
+    objects = models.Manager()  # default manager
+    postobjects = PostObjects()  # custom manager
+
+    class Meta:
+        ordering = ('-published',)
+
+        def __str__(self) -> str:
+            return self.title
